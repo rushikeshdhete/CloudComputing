8734c6f25a88436b0e3b30769b66238d54aecde0
rushikeshdhete <rushikeshdhete@gmail.com>
2022-03-17 19:25:12 +0530

    Update

--- Login2.O/src/main/java/login/Login.java
+++ Login2.O/src/main/java/login/Login.java
@@ -1,41 +1,48 @@
 package login;
 
-import java.io.IOException;
+import java.sql.*;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class Login
- */
+import Dao.LoginDao;
+
+import java.io.IOException;
+
+
 @WebServlet("/Login")
-public class Login extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+public class login extends HttpServlet {
+
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
+    
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * 
 	 */
+	private static final long serialVersionUID = 1L;
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+		String uname=request.getParameter("uname");
+		String pass=request.getParameter("pass");
+		
+		LoginDao dao=new LoginDao();
+		
+		if(dao.checkcredit(uname, pass))
+		{
+			HttpSession session=request.getSession();
+			session.setAttribute("uname",uname);
+			 response.sendRedirect("home.jsp");
+		}
+		else
+		{
+			response.sendRedirect("login.jsp");
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	
 
 }
